0363183bb680f540ccfb331be7dd33dc5f632b12
anttiharju <anttiharju@users.noreply.github.com>
2026-05-03 11:59:36 +0000

    compare-changes 0.8.16

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 printf 'compare-changes --wildcard "%s" --changes "%s"\n' "$WILDCARD" "$CHANGES"
 debug_flag=""
-if [ "$DEBUG" = "true" ]; then
+if [[ "$DEBUG" = "true" ]]; then
   debug_flag="--debug"
 fi
 compare-changes --wildcard "$WILDCARD" --changes "$CHANGES" $debug_flag
